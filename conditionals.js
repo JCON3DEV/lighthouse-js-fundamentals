3947f37ef4073a8c34f26b41e5d244d4545ba1e0
JCON3DEV <jconheasman@gmail.com>
2020-06-13 21:24:28 +0000

    first commit. Basic if statement

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
