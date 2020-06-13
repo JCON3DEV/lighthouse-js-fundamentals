02231b8b504a351df1728e89db63336606194c60
JCON3DEV <jconheasman@gmail.com>
2020-06-13 21:56:12 +0000

    third commit. Finished if statement. changed age conditional statements and style / spacing

---
+++ whichSchool.js
@@ -0,0 +1,12 @@
+const whichSchool = function(age) {
+  if (age <= 13){
+    console.log("Elementary School");
+  }else if(age >= 14 && age <= 18){
+    console.log("Secondary School");
+  }else {
+    console.log("Lighthouse Labs");
+  }
+};
+
+console.log("I am 18. Which school should I go to?");
+whichSchool(18);
