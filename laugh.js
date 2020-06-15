48662fb9114b05ca4b55f7a0da41db02c65c5f33
JCON3DEV <jconheasman@gmail.com>
2020-06-15 19:54:31 +0000

    improved the laugh.js function to take argument as to length of laugh

--- laugh.js
+++ laugh.js
@@ -1,5 +1,24 @@
-function laugh(){
-  let lol = "hahahahahahahahahaha";
+let lol = "";
+
+function laugh(num){
+  for (var x = 0; x < num ; ++x){    
+    lol = lol + "ha";     
+  }
   return lol;
+  
 }
-console.log(laugh()+ "!"); 
+console.log(laugh(3)+ "!"); 
+
+/* ## Udacity class solution;
+
+var sound = "";
+function laugh(num) {
+  for (var x = 0; x < num; x++) {
+    sound = sound + "ha";
+  }
+  sound = sound + "!";
+  return sound;
+}
+
+console.log(laugh(3));
+*/
