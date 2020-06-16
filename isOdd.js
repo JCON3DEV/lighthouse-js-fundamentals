0853d3a145ace00a6162a7d2f1312592a5db5817
JCON3DEV <jconheasman@gmail.com>
2020-06-16 18:54:50 +0000

    added a js file including a function for finding odd numbers

---
+++ isOdd.js
@@ -0,0 +1,11 @@
+function isOdd (num){
+  if(num % 2 === 0){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+console.log("3 is odd; " + isOdd(3));
+console.log("8 is odd; " + isOdd(8));
