3cdb28c7a038732cd542b92ae90784710e27e49a
JCON3DEV <jconheasman@gmail.com>
2020-06-21 20:28:43 +0000

    updated packing-list.js to use a while loop instead of a for loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,18 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+/*
+
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+*/
+//Practice with a while statement to loop through an array;
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i< packingList.length){
+  console.log(packingList[i]);
+  i++;
 }
